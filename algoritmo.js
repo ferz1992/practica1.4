ac6c6c6c181c62b7099a08567caf81ca79a48d4d
Fernando Zalazar <f.zalazar@live.com>
2022-04-08 08:10:37 -0300

    practica 1.4

---
+++ algoritmo.js
@@ -0,0 +1,20 @@
+document.write( `
+<details>
+        <summary> info 1 
+        </summary>
+        <p>texto de detalle</p>
+
+        <img src="./borrar-fondo-imagen.jpg" alt="imagen random" width="800px" height="400px">
+
+        <br>
+
+        <a href="https://www.1001juegos.com/e/retro" > juegos retro </a>
+
+    </details>
+    <details>
+        <summary>info 2</summary>
+        
+        <p>texto de detalles</p>
+    </details>
+
+`);
